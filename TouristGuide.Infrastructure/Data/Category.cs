305f4825feccdd764429473e7c8a0d73f915c3ce
Katerina Kotseva <k.kotseva@xs-software>
2022-12-18 22:30:30 +0200

    Added places and categories

---
+++ TouristGuide.Infrastructure/Data/Category.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TouristGuide.Infrastructure.Data
+{
+    internal class Category
+    {
+        public Category()
+        {
+            Places = new List<Place>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; } = null!;
+
+        public List<Place> Places { get; set; }
+    }
+}
