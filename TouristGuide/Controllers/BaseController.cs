ebef06ed322bbdb7bd2c52fdef37700ac248727d
Katerina Kotseva <k.kotseva@xs-software>
2022-12-19 21:08:06 +0200

    Added some empty controllers

---
+++ TouristGuide/Controllers/BaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TouristGuide.Controllers
+{
+    public class BaseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
